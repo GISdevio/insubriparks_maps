6bc001f308fc99e17867b32a4c83e825ab48b312
matteorizziWT <75792655+matteorizziWT@users.noreply.github.com>
2021-09-18 13:02:13 +0200

    from geojson data to js file

---
+++ assets/js/areoporti.js
@@ -0,0 +1,56 @@
+let areoporti = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "id": 1,
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    8.725199968000027,
+                    45.62865053100006
+                ]
+            },
+            "properties": {
+                "fid": 1,
+                "nome": "Milano-Malpensa",
+                "id": 1,
+                "info": null
+            }
+        },
+        {
+            "type": "Feature",
+            "id": 2,
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    8.910193701000026,
+                    46.00413724600003
+                ]
+            },
+            "properties": {
+                "fid": 2,
+                "nome": "Lugano-Agno",
+                "id": 3,
+                "info": null
+            }
+        },
+        {
+            "type": "Feature",
+            "id": 3,
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    9.273995143000036,
+                    45.45120127400003
+                ]
+            },
+            "properties": {
+                "fid": 3,
+                "nome": "Milano-Linate",
+                "id": 2,
+                "info": null
+            }
+        }
+    ]
+}
