35736aed65b1dd14d4d2ef412c70f8b20ddd654e
matteorizziWT <75792655+matteorizziWT@users.noreply.github.com>
2021-10-02 16:16:59 +0200

    image and markers

---
+++ config.js
@@ -0,0 +1,18 @@
+var appConfig = {
+    'basemapImage': "basemap.png",
+    'parkMarker': "parkMarker.svg",
+    'parks': {
+        'spinaVerde':{
+            'nome': "Spina Verde",
+            'url': "https://www.spinaverde.it/main/"
+        },
+        'pineta': {
+            'nome': "Parco Pineta",
+            'url': "http://www.parcopineta.org/"
+        },
+        'style': {
+            'color': 'white',
+            'backgroundColor': 'green'
+        }
+    }
+}
